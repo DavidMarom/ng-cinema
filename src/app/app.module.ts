67b66bbf498d364ec9de10483842140d76ec60f9
David Marom <davidmarom.pro@gmail.com>
2024-03-09 17:00:32 +0200

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page01Component } from './page01/page01.component';
+import { FormsModule } from '@angular/forms';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { CatalogComponent } from './catalog/catalog.component';
 
+const routes: Routes = [
+  { path: 'catalog', component: CatalogComponent },
+
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page01Component,
+    SidebarComponent,
+    CatalogComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
